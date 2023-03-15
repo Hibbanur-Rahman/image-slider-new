6454929e6100bff65ff67700726bc5964c02590b
HIBBANUR RAHMAN <117516111+Hibbanur-Rahman@users.noreply.github.com>
2023-03-16 02:56:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+var slides1 = document.querySelectorAll(".slide");
+var counter = 0;
+console.log(slides1);
+// slides1.forEach((slide, indx) => {
+//     slide.style.transform = `translateX(${indx * 100}%)`;
+//   });
+var goNext = () => {
+    counter++;
+    if(counter>=slides1.length)
+    {
+        counter=0;
+    }
+    slideImage();
+    console.log(counter);
+}
+var goPrev = () => {
+    counter--;
+    if(counter<0)
+    {
+        counter=slides1.length-1;
+    }
+    slideImage();
+    console.log(counter);
+}
+var slideImage=()=>{
+    slides1.forEach(
+        (slide)=>{
+            slide.style.transform=`translateX(-${counter*100}%)`
+        }
+    )
+}
